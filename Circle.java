bb762baaed3337ae9edaea6d7dead16178132c35
Azad Ayhan <azadayhan50@gmail.com>
2025-04-08 16:52:28 +0300

    Ex: Yet Another Circle Class

--- Circle.java
+++ Circle.java
@@ -1,43 +1,31 @@
 public class Circle {
     private double radius;
-    private String color;
 
     public Circle() {
         radius = 1.0;
-        color = "red";
     }
 
     public Circle(double radius) {
         this.radius = radius;
-        color = "red";
-    }
-
-    public Circle(double radius, String c) {
-        this.radius = radius;
-        color = c;
     }
 
     public double getRadius() {
         return radius;
     }
 
-    public double getArea() {
-        return Math.PI * radius * radius;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
     public void setRadius(double radius) {
         this.radius = radius;
     }
 
-    public void setColor(String color) {
-        this.color = color;
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    public double getCircumference() {
+        return 2 * Math.PI * radius;
     }
 
     public String toString() {
-        return "Circle[radius=" + radius + " color=" + color + "]";
-     }
+        return "Circle[radius=" + radius + "]";
+    }
 }
