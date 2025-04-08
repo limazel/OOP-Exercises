5ea98e23165f0e8246bfdd6beb076dc936763456
Azad Ayhan <azadayhan50@gmail.com>
2025-04-08 21:42:54 +0300

    An Introduction to OOP Inheritance by Example

--- Circle.java
+++ Circle.java
@@ -1,12 +1,20 @@
 public class Circle {
     private double radius;
+    private String color;
 
     public Circle() {
         radius = 1.0;
+        color = "red";
     }
 
     public Circle(double radius) {
         this.radius = radius;
+        color = "red";
+    }
+
+    public Circle(double radius, String color) {
+        this.radius = radius;
+        this.color = color;
     }
 
     public double getRadius() {
@@ -17,15 +25,19 @@ public class Circle {
         this.radius = radius;
     }
 
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
     public double getArea() {
         return Math.PI * radius * radius;
     }
 
-    public double getCircumference() {
-        return 2 * Math.PI * radius;
-    }
-
     public String toString() {
-        return "Circle[radius=" + radius + "]";
+        return "Circle[radius" + radius + ", color" + color + "]";
     }
 }
